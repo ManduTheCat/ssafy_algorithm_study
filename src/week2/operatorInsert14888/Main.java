28b7291a10642ab6da2410e04f2b539244acc378
fortuna3co <fortuna3co@gmail.com>
2022-08-12 18:05:04 +0900

    ans

--- src/week2/operatorInsert14888/Main.java
+++ src/week2/operatorInsert14888/Main.java
@@ -1,4 +1,47 @@
-package week2.operatorInsert14888;
+package Day_0812;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class BJ_1931_회의실배정_강혜성 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		PriorityQueue<int[]> pq = new PriorityQueue<int[]>(new Comparator<int[]>(){
+
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				if (o1[1] == o2[1]) {
+					return Integer.compare(o1[0], o2[0]);
+				}
+				return Integer.compare(o1[1], o2[1]);
+			}
+			
+		});
+		
+		for(int i = 0; i < N; ++i) {
+			st = new StringTokenizer(br.readLine());
+			int[] temp = {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+			pq.add(temp);
+		}
+		
+		int count = 1;
+		int[] time = pq.poll();
+		while(!pq.isEmpty()) {
+			
+			int[] temp = pq.poll();
+			if (time[1] <= temp[0]) {
+				time = temp;
+				count++;
+			}
+		}
+		
+		System.out.println(count);
+		
+	}
 }
