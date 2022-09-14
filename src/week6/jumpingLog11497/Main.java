4ad9ca1be4ab8f400bb5bc6728428caa11a212ff
gkatldus1 <gkatldus1@gmail.com>
2022-09-14 19:41:21 +0900

    통나무건너건너 BJ11497

--- src/week6/jumpingLog11497/Main.java
+++ src/week6/jumpingLog11497/Main.java
@@ -1,4 +1,45 @@
-package week6.jumpingLog11497;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Main {
+public class Baek11497 {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int T = Integer.parseInt(br.readLine());
+
+    for (int tc = 1; tc <= T; tc++) {
+      int N = Integer.parseInt(br.readLine());
+      int max = Integer.MIN_VALUE;
+      int[] arr = new int[N];
+      int[] ans = new int[N];
+
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      for (int i = 0; i < N; i++) {
+        arr[i] = Integer.parseInt(st.nextToken());
+      }
+      Arrays.sort(arr);
+      int left = 0;
+      int right = N - 1;
+      int idx = 0;
+      while (left <= right) {
+        if (right == left) {
+          ans[left] = arr[idx];
+          break;
+        }
+        ans[left] = arr[idx++];
+        ans[right] = arr[idx++];
+        left++;
+        right--;
+      }    
+     
+      for (int i = 0; i < N - 1; i++) {
+        int temp = Math.abs(ans[i] - ans[i + 1]);
+        max = Math.max(temp, max);
+      }
+      int temp = Math.abs(ans[0] - ans[N - 1]);
+      max = Math.max(temp, max);
+      System.out.println(max);
+    }
+  }
 }
